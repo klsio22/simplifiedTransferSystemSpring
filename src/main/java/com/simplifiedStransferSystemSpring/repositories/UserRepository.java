a35dc1bf74ab2a55b86299be1d6ed46b8aa9a1eb
Klesio <klesio.dev@gmail.com>
2026-01-24 18:17:59 -0300

    Add UserRepository interface and configure H2 database settings

---
+++ src/main/java/com/simplifiedStransferSystemSpring/repositories/UserRepository.java
@@ -0,0 +1,16 @@
+package com.simplifiedStransferSystemSpring.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.simplifiedStransferSystemSpring.domain.user.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findUserByDocument(String document);
+
+    Optional<User> findUserById(Long id);
+}
