7428969e66b2e136e0ae138be0e8caded4b06d9c
Klesio <klesio.dev@gmail.com>
2026-01-24 00:13:16 -0300

    first commit

---
+++ src/main/java/com/simplifiedStransferSystemSpring/SimplifiedStransferSystemSpringApplication.java
@@ -0,0 +1,13 @@
+package com.simplifiedStransferSystemSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplifiedStransferSystemSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplifiedStransferSystemSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplifiedStransferSystemSpring/SimplifiedStransferSystemSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplifiedStransferSystemSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplifiedStransferSystemSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
