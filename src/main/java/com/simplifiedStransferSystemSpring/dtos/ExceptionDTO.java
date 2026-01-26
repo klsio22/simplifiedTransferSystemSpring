c7a5d6de234719af0800e960c7a93aa02d5b1a46
Klesio <klesio.dev@gmail.com>
2026-01-26 00:14:26 -0300

    Add ExceptionDTO and ControllerExceptionHandler for centralized exception handling

---
+++ src/main/java/com/simplifiedStransferSystemSpring/dtos/ExceptionDTO.java
@@ -0,0 +1,4 @@
+package com.simplifiedStransferSystemSpring.dtos;
+
+public record ExceptionDTO(String message, String statusCode) {
+}
---
+++ src/main/java/com/simplifiedStransferSystemSpring/infra/ControllerExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.simplifiedStransferSystemSpring.infra;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.simplifiedStransferSystemSpring.dtos.ExceptionDTO;
+
+import jakarta.persistence.EntityNotFoundException;
+
+@RestController
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<ExceptionDTO> threatDuplicateEntity(DataIntegrityViolationException exception) {
+        ExceptionDTO exceptionDTO = new ExceptionDTO("User duplicate", "400");
+        return ResponseEntity.badRequest().body(exceptionDTO);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ExceptionDTO> threat404(EntityNotFoundException exception) {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ExceptionDTO> threatGeneralException(Exception exception) {
+        ExceptionDTO exceptionDTO = new ExceptionDTO("Internal server error", "500");
+        return ResponseEntity.internalServerError().body(exceptionDTO);
+    }
+
+}
